99ca6d11966cd4d8661ea56777891d01e5ce800b
ethanschafstall <ethans9999@gmail.com>
2024-04-23 16:34:20 +0200

    finished exo

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,8 @@ import { RouterLink, RouterView } from 'vue-router'
       <div class="wrapper">
         <!-- <HelloWorld msg="You did it!" /> -->
         <nav>
-          <RouterLink to="/">Events</RouterLink> |
-          <RouterLink to="/about">About</RouterLink>
+          <RouterLink :to="{ name: 'event-list' }">Events</RouterLink> |
+          <RouterLink :to="{ name: 'about' }">About</RouterLink>
         </nav>
       </div>
     </header>
