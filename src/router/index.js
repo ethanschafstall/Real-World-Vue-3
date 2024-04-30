e87ffcbe7efd8dfe7ba3b2c1205d7786a36b43b0
ethanschafstall <ethans9999@gmail.com>
2024-04-30 15:51:02 +0200

    updated exercies

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,8 @@ const router = createRouter({
     {
       path: '/',
       name: 'event-list',
-      component: EventListView
+      component: EventListView,
+      props: (route) => ({ page: parseInt(route.query.page) || 1 })
     },
     {
       path: '/about',
@@ -26,6 +27,12 @@ const router = createRouter({
       props: true,
       component: EventDetailsView
     }
+    // ,
+    // {
+    //   path: '/event',
+    //   component: EventListView,
+    //   props: (route) => ({ showExtra: route.query.e })
+    // }
   ]
 })
 
--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,12 +1,15 @@
 <script setup>
 import EventCard from '@/components/EventCard.vue'
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, computed } from 'vue'
 import EventService from '@/services/EventService.js'
+
+const props = defineProps(['page'])
+const page = computed(() => props.page)
 
 const events = ref(null)
 
 onMounted(() => {
-  EventService.getEvents()
+  EventService.getEvents(2, page.value)
     .then((reponse) => {
       events.value = reponse.data
     })
@@ -19,6 +22,8 @@ onMounted(() => {
 <template>
   <main>
     <h1>Events For Good</h1>
+    <!-- <h1>You are on page {{ page }}</h1> -->
+    <h1 v-show="showExtra">Extra Stuff</h1>
     <div class="events">
       <EventCard v-for="event in events" :key="event.id" :event="event" />
     </div>
